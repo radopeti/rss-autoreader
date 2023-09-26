712d7352514ba37e3951ab3b0160050c3fc613ce
Peter Rado <rado.peti.subs@gmail.com>
2023-09-26 21:51:33 +0200

    add start stop functions

--- src/app/reader/reader.component.ts
+++ src/app/reader/reader.component.ts
@@ -9,21 +9,35 @@ import { FeedClientService } from '../services/feed-client.service';
 export class ReaderComponent implements OnInit {
 
   currentWord: string = "";
+  private words : Array<string> = []
+  private currentIndex = 0;
+  private isPlaying : boolean = false;
+  private intervalId : any = null;
 
   constructor(private feedClient: FeedClientService) {}
 
   ngOnInit() {
-    //this.setCurrentWord();
-    let currentIndex = 1;
     this.feedClient.getFeed("https://danaepp.com/feed").subscribe(article => {
-      console.log(article)
-      let words = article.message.split(" ")
-      console.log(words)
-      setInterval(() => {
-        this.currentWord = words[currentIndex];
-        currentIndex = (currentIndex + 1) % words.length;
-      }, 300);
+      this.words = article.message.split(" ");
     });
+  }
+
+  start() {
+    if (this.isPlaying) return;
+
+    this.isPlaying = true;
+    console.log("started")
+    this.intervalId = setInterval(() => {
+      this.currentWord = this.words[this.currentIndex];
+      this.currentIndex = (this.currentIndex + 1) % this.words.length;
+      console.log(this.isPlaying)
+    }, 300);
+  }
+
+  stop() {
+    console.log("stop")
+    this.isPlaying = false;
+    clearInterval(this.intervalId)
   }
 
 }
